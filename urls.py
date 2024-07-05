b8709bf3044ca140e8180a67d7f43b8a5389dfd0
morifusa <58528158+morifusa@users.noreply.github.com>
2024-07-05 12:07:14 -0700

    Added user management files.

---
+++ urls.py
@@ -0,0 +1,31 @@
+""" Define URL patterns for users."""
+
+# from django.urls import path
+# from django.contrib.auth.views import Login
+
+# from . import views
+
+# app_name = "users"
+# urlpatterns = [
+#     # Login page
+#     path('login/', login, {'template_name' : 'users/login.html'}, name = "login"),
+# ]
+
+
+from django.urls import path
+from django.contrib.auth.views import LoginView
+
+from . import views
+
+app_name = "users"
+urlpatterns = [
+    # Login page
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+
+    # Logout page
+    path('logout/', views.logout_view, name='logout'),
+
+    # Registration page
+    path('register/', views.register, name='register'),
+
+]
